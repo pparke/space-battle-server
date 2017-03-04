f614ab83b8ff35549197cf434c57a564e155caba
Philip Parke <philipparke@gmail.com>
2017-03-04 10:37:53 -0400

    contours

--- src/index.js
+++ src/index.js
@@ -8,8 +8,17 @@ const app = new Koa();
 const server = http.createServer(app.callback());
 
 //app.use(serve('/'));
-const COLOR = [0, 255, 0]; // default red
+const BLUE = [0, 255, 0]; // B, G, R
+const RED = [0, 0, 255]; // B, G, R
+const GREEN = [0, 255, 0]; // B, G, R
+const WHITE = [255, 255, 255]; // B, G, R
 const thickness = 2; // default 1
+const lowThresh = 0;
+const highThresh = 100;
+const nIters = 2;
+const minArea = 2000;
+
+let camera;
 
 app.listen(3000);
 
@@ -20,16 +29,49 @@ io.on('connection', (socket) => {
   console.log('connection!');
   socket.emit('msg', 'hello from the socket!');
   try {
-    const camera = new cv.VideoCapture(0);
+    if (!camera) {
+      camera = new cv.VideoCapture(0);
+    }
+    // const out = new cv.Matrix(HEIGHT, WIDTH);
 
     setInterval(() => {
       camera.read((err, im) => {
         if (err) {
           throw err;
         }
-        if (im.size()[0] > 0 && im.size()[1] > 0) {
-          socket.emit('frame', { buffer: im.toBuffer() });
 
+        const [WIDTH, HEIGHT] = im.size();
+
+        if (WIDTH > 0 && HEIGHT > 0) {
+          const out = im.copy();
+
+          // contour detection
+          const contourIm = im.copy();
+          contourIm.convertGrayscale();
+          contourIm.canny(lowThresh, highThresh);
+          contourIm.dilate(nIters);
+          const contours = contourIm.findContours();
+
+          for (let i = 0; i < contours.size(); i++) {
+            if (contours.area(i) < minArea) {
+              continue;
+            }
+
+            const arcLength = contours.arcLength(i, true);
+            contours.approxPolyDP(i, 0.01 * arcLength, true);
+            switch(contours.cornerCount(i)) {
+              case 3:
+                out.drawContour(contours, i, GREEN);
+                break;
+              case 4:
+                out.drawContour(contours, i, RED);
+                break;
+              default:
+                out.drawContour(contours, i, WHITE);
+            }
+          }
+
+          // face detection
           im.detectObject(cv.FACE_CASCADE, {}, (err2, faces) => {
             if (err2) {
               throw err2;
@@ -37,11 +79,11 @@ io.on('connection', (socket) => {
             if (faces.length > 0) {
               for (let i = 0; i < faces.length; i++) {
                 const face = faces[i];
-                im.rectangle([face.x, face.y], [face.width, face.height], COLOR, 2);
+                out.rectangle([face.x, face.y], [face.width, face.height], BLUE, 2);
               }
             }
 
-             socket.emit('frame', { buffer: im.toBuffer() });
+             socket.emit('frame', { buffer: out.toBuffer() });
           });
         }
       });
